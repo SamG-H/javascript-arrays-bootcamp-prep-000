5eaa7d7e35d0d1a9d35190118d689e762bb711cb
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-05-02 19:15:53 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,9 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
-function addElementToBeginningOfArray(){
+function addElementToBeginningOfArray(arr, element){
   
 }
 
-function destrucivelyAddElementToBeginningOfArray(){
+function destrucivelyAddElementToBeginningOfArray(arr, element){
   
 }
