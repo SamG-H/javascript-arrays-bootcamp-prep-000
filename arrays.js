45dca6cf81c2961ef97c63c7028733a469dc6f8e
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-05-02 19:17:55 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,10 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
-function addElementToBeginningOfArray(arr, element){
-  
+function addElementToBeginningOfArray(oldArr, element){
+  var newArr = oldArr;
+  return newArr.unshift(element);
 }
 
 function destrucivelyAddElementToBeginningOfArray(arr, element){
-  
+  return arr.unshift(element)
 }
